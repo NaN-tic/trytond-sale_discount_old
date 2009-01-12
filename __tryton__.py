4a589a39d91bcf8aba211180be732a14c2efc242
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-01-12 13:34:10 +0100

    Add translation for de_DE Modify descriptions and add german translation

--- __tryton__.py
+++ __tryton__.py
@@ -1,16 +1,17 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    'name': 'Sale Order Discount',
-    'name_de_DE': 'Rabattspalte für Angebot/Auftrag',
+    'name': 'Sales Discount',
+    'name_de_DE': 'Verkauf Rabatt',
     'version': '0.0.1',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
-    'description': '''This module adds a discount column on sale order
+    'description': '''Define discounts for sale lines
+    - with additional field discount in report sale
 ''',
-    'description_de_DE': '''Dieses Modul fügt dem Angebots- und Auftragsformular eine
-    Rabattspalte hinzu
+    'description_de_DE': '''Ermöglicht die Eingabe von Rabatten pro Verkaufsposition
+    - mit zusätzlichem Rabattfeld im Bericht Verkauf
 ''',
     'depends': [
         'sale',
@@ -19,7 +20,7 @@
     'xml': [
         'sale.xml'
         ],
-#    'translation': [
-#        'de_DE.csv'
-#    ],
+    'translation': [
+        'de_DE.csv'
+    ],
 }
