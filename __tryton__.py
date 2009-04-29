a002498d360134f775bbd664d7429e44cf829b1b
Korbinian Preisler <timitos@virtual-things.biz>
2009-04-29 11:02:15 +0200

    set description for public beta status

--- __tryton__.py
+++ __tryton__.py
@@ -1,18 +1,28 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 {
-    'name': 'Sales Discount',
-    'name_de_DE': 'Verkauf Rabatt',
+    'name': 'BETA: Sales Discount',
+    'name_de_DE': 'BETA: Verkauf Rabatt',
     'version': '1.1.0',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
-    'description': '''Define discounts for sale lines
-    - with additional field discount in report sale
+    'description': '''WARNING: BETA STATUS
+This module is in public testing phase and not yet released.
+Never use this module in productive environment. You can not
+uninstall this module once it is installed. Watch
+www.tryton.org/news.html for release announcements.
+
+Use this module only for testing purposes and submit your issues to
+http://bugs.tryton.org. Please note your testing results on
+http://code.google.com/p/tryton/wiki/Testing1_2_0#External_Modules.
+
+Define discounts for sale lines
+- with additional field discount in report sale
 ''',
-    'description_de_DE': '''Ermöglicht die Eingabe von Rabatten pro Verkaufsposition
-    - mit zusätzlichem Rabattfeld im Bericht Verkauf
-''',
+    #'description_de_DE': '''Ermöglicht die Eingabe von Rabatten pro Verkaufsposition
+    #- mit zusätzlichem Rabattfeld im Bericht Verkauf
+#''',
     'depends': [
         'sale',
         'account_invoice_discount'
