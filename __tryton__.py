972851646f800e1c5e22675d34c67a46517b1722
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-04-30 00:24:40 +0200

    Update translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
-    'description': '''WARNING: BETA STATUS
+    'description': '''        WARNING: BETA STATUS
 This module is in public testing phase and not yet released.
 Never use this module in productive environment. You can not
 uninstall this module once it is installed. Watch
@@ -20,9 +20,21 @@ http://code.google.com/p/tryton/wiki/Testing1_2_0#External_Modules.
 Define discounts for sale lines
 - with additional field discount in report sale
 ''',
-    #'description_de_DE': '''Ermöglicht die Eingabe von Rabatten pro Verkaufsposition
-    #- mit zusätzlichem Rabattfeld im Bericht Verkauf
-#''',
+    'description_de_DE': '''        ACHTUNG: MODUL IM BETA STATUS
+        Dieses Modul befindet sich in der öffentlichen Testphase und
+        ist noch nicht released. Benutzen Sie dieses Modul nicht in
+        produktiven Umgebungen. Das Modul kann nicht mehr deinstalliert werden,
+        nachdem es in eine Datenbank installiert wurde.
+        Release Ankündigung werden auf www.tryton.org/news.html veröffentlicht.
+
+        Benutzen Sie dieses Modul nur für Testzwecke und unterbreiten Sie
+        Fehler oder Vorschläge auf http://bugs.tryton.org. Bitte vermerken Sie
+        Ihre Testergebnisse auf
+        http://code.google.com/p/tryton/wiki/Testing1_2_0#External_Modules.
+
+    Ermöglicht die Eingabe von Rabatten pro Verkaufsposition
+    - mit zusätzlichem Rabattfeld im Bericht Verkauf
+''',
     'depends': [
         'sale',
         'account_invoice_discount'
