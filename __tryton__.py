04905a9295af02c2cfa132e814af95e665b6a733
Mathias Behrle <mbehrle@m9s.biz>
2010-10-02 20:56:57 +0200

    Prepare release 1.4.0

--- __tryton__.py
+++ __tryton__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Sale Discount',
     'name_de_DE': 'Verkauf Rabatt',
-    'version': '1.1.0',
+    'version': '1.4.0',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
