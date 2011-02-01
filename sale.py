78da0b7a46b9ed35885a9ee3aa888ee50a8ab519
Udo Spallek <udono@virtual-things.biz>
2011-02-01 15:42:53 +0100

    Update for new Transaction

--- sale.py
+++ sale.py
@@ -1,9 +1,12 @@
 #This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
+from __future__ import with_statement
+import copy
+from decimal import Decimal
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pyson import Not, Equal, Eval
-from decimal import Decimal
-import copy
+from trytond.transaction import Transaction
+
 
 class SaleLine(ModelSQL, ModelView):
     _name = 'sale.line'
@@ -18,34 +21,31 @@ class SaleLine(ModelSQL, ModelView):
             self.amount.on_change_with.extend(['discount'])
         self._reset_columns()
 
-    def default_discount(self, cursor, user, context=None):
+    def default_discount(self):
         return 0.0
 
-    def on_change_with_amount(self, cursor, user, vals, context=None):
+    def on_change_with_amount(self, vals):
         if vals.get('type') == 'line' and vals.get('discount'):
             vals = vals.copy()
             vals['unit_price'] = (vals.get('unit_price') -
                 vals.get('unit_price') * vals.get('discount') * Decimal('0.01'))
-        return super(SaleLine, self).on_change_with_amount(cursor, user,
-                                                vals, context=context)
+        return super(SaleLine, self).on_change_with_amount(vals)
 
-    def get_amount(self, cursor, user, ids, name, context=None):
+    def get_amount(self, ids, name):
         currency_obj = self.pool.get('currency.currency')
-        res = super(SaleLine, self).get_amount(cursor, user, ids, name,
-                                                  context=context)
-        for line in self.browse(cursor, user, ids, context=context):
+        res = super(SaleLine, self).get_amount(ids, name)
+        for line in self.browse(ids):
             if line.type == 'line':
                 currency = line.sale and line.sale.currency \
                         or line.currency
-                res[line.id] = currency_obj.round(cursor, user, currency, \
-                        Decimal(str(line.quantity)) * line.unit_price - \
-                            (Decimal(str(line.quantity)) * line.unit_price *\
+                res[line.id] = currency_obj.round(currency,
+                        Decimal(str(line.quantity)) * line.unit_price -
+                            (Decimal(str(line.quantity)) * line.unit_price *
                             (line.discount * Decimal('0.01'))))
         return res
 
-    def get_invoice_line(self, cursor, user, line, context=None):
-        res = super(SaleLine, self).get_invoice_line(cursor, user, line,
-                                                     context=context)[0]
+    def get_invoice_line(self, line):
+        res = super(SaleLine, self).get_invoice_line(line)[0]
         if line.type != 'line':
             return [res]
         if res['quantity'] <= 0.0:
@@ -59,7 +59,7 @@ SaleLine()
 class Sale(ModelSQL, ModelView):
     _name = 'sale.sale'
 
-    def on_change_lines(self, cursor, user, vals, context=None):
+    def on_change_lines(self, vals):
         if vals.get('lines'):
             vals = vals.copy()
             lines = []
@@ -70,41 +70,34 @@ class Sale(ModelSQL, ModelView):
                               Decimal('0.01'))
                 lines.append(line)
             vals['lines'] = lines
-        return super(Sale, self).on_change_lines(cursor, user, vals,
-                                                 context=context)
+        return super(Sale, self).on_change_lines(vals)
 
-    def get_tax_amount(self, cursor, user, sales, context=None):
+    def get_tax_amount(self, sales):
         '''
         Compute tax amount for each sales
 
-        :param cursor: the database cursor
-        :param user: the user id
         :param sales: a BrowseRecordList of sales
-        :param context: the context
         :return: a dictionary with sale id as key and
             tax amount as value
         '''
         currency_obj = self.pool.get('currency.currency')
         tax_obj = self.pool.get('account.tax')
-        if context is None:
-            context = {}
         res = {}
         for sale in sales:
-            ctx = context.copy()
-            ctx.update(self.get_tax_context(cursor, user, sale,
-                context=context))
+            context = self.get_tax_context(sale)
             res.setdefault(sale.id, Decimal('0.0'))
             for line in sale.lines:
                 if line.type != 'line':
                     continue
                 price = line.unit_price - line.unit_price * line.discount / Decimal('100')
                 # Don't round on each line to handle rounding error
-                for tax in tax_obj.compute(cursor, user,
-                        [t.id for t in line.taxes], price,
-                        line.quantity, context=ctx):
+                with Transaction().set_context(**context):
+                    taxes_compute = tax_obj.compute(
+                            [t.id for t in line.taxes], price, line.quantity)
+
+                for tax in taxes_compute:
                     res[sale.id] += tax['amount']
-            res[sale.id] = currency_obj.round(cursor, user, sale.currency,
-                    res[sale.id])
+            res[sale.id] = currency_obj.round(sale.currency, res[sale.id])
         return res
 
 Sale()
