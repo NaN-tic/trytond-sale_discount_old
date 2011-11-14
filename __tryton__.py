fd3b73f815bbfd00d81353f5852efb0989f99035
Mathias Behrle <mbehrle@m9s.biz>
2011-11-14 19:22:23 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Sale Discount',
     'name_de_DE': 'Verkauf Rabatt',
-    'version': '2.0.0',
+    'version': '2.1.0',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
