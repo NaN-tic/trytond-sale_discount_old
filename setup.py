db0161f58cf978049af5b5efee447e3c9d8339fa
Mathias Behrle <mbehrle@m9s.biz>
2011-11-16 12:54:40 +0100

    Set Tryton classifier

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(name='trytond_sale_discount',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Plugins',
+        'Framework :: Tryton',
         'Intended Audience :: Developers',
         'Intended Audience :: Financial and Insurance Industry',
         'Intended Audience :: Legal Industry',
