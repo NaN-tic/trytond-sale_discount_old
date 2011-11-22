93982315b436bc1b5919f77feda1ec48d1336897
Udo Spallek <udono@virtual-things.biz>
2011-11-22 10:21:48 +0000

    Add test for depends

--- tests/test_sale_discount.py
+++ tests/test_sale_discount.py
@@ -10,7 +10,7 @@ if os.path.isdir(DIR):
 
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view
+from trytond.tests.test_tryton import test_view, test_depends
 
 
 class SaleDiscountTestCase(unittest.TestCase):
@@ -26,6 +26,12 @@ class SaleDiscountTestCase(unittest.TestCase):
         Test views.
         '''
         self.assertRaises(Exception, test_view('sale_discount'))
+
+    def test0006depends(self):
+        '''
+        Test depends.
+        '''
+        test_depends()
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
