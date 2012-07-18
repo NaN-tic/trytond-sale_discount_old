cbe512b4a6c46a8803b6384cb94642301480187a
resteve <resteve@zikzakmedia.com>
2012-07-18 11:09:01 +0200

    Change copyright notice files

--- sale.py
+++ sale.py
@@ -1,5 +1,6 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+#This file is part sale_discount module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
 import copy
 from decimal import Decimal
 from trytond.model import ModelView, ModelSQL, fields
