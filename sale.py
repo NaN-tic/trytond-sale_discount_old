39183faddcd0153d371d11244018925f73ed843e
resteve <resteve@zikzakmedia.com>
2012-10-18 12:43:25 +0200

    Clean some import not necessary

--- sale.py
+++ sale.py
@@ -1,11 +1,9 @@
 #This file is part sale_discount module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-import copy
 from decimal import Decimal
 from trytond.model import fields
 from trytond.pyson import Not, Equal, Eval
-from trytond.transaction import Transaction
 from trytond.pool import Pool, PoolMeta
 
 __all__ = ['SaleLine']
