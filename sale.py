c5a3993e81999c793f7a8196d725a95581883cb7
resteve <resteve@zikzakmedia.com>
2012-10-18 13:17:53 +0200

    FIX one change discount when not quantity available

--- sale.py
+++ sale.py
@@ -43,9 +43,10 @@ class SaleLine:
         res = {}
         if self.discount == Decimal(0.0) and self.quantity != None:
             res['amount'] = Decimal(self.quantity)*self.unit_price
-        if self.discount and self.unit_price and self.type == 'line':
-            res['amount'] = Decimal(self.quantity)*(self.unit_price -
-                self.unit_price * self.discount * Decimal('0.01'))
+        if self.discount and self.unit_price and self.type == 'line' \
+            and self.quantity != None:
+                res['amount'] = Decimal(self.quantity)*(self.unit_price -
+                    self.unit_price * self.discount * Decimal('0.01'))
         return res
 
     def on_change_product(self):
